40ebeebe5111b4131d05469a597bc8066f33e649
Name <vikafantasy@mail.ru>
2015-02-27 08:42:24 +0300

    Initial Commit

---
+++ test2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  test2
+//
+//  Created by Victoria on 27.02.15.
+//  Copyright (c) 2015 Victoria. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
